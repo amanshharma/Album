d14e9141dc2d8f0ab87d827de933e02155979326
Aman Sharma <amandeep.xml@gmail.com>
2019-06-30 01:30:49 -0400

    Added the album image and updated to make the list scrollable

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -5,12 +5,13 @@ import Card from "./Card";
 import CardSection from "./CardSection";
 
 const AlbumDetail = ({ album }) => {
-  const { thumbnail_image, title, artist } = album;
+  const { thumbnail_image, title, artist, image } = album;
   const {
     imageThumbnailStyle,
     headerContentStyle,
     thumbnailContainerStyle,
-    headerTextStyle
+    headerTextStyle,
+    imageStyle
   } = styles;
 
   return (
@@ -26,6 +27,9 @@ const AlbumDetail = ({ album }) => {
           <Text style={headerTextStyle}>{title}</Text>
           <Text>{artist}</Text>
         </View>
+      </CardSection>
+      <CardSection>
+        <Image source={{ uri: image }} style={imageStyle} />
       </CardSection>
     </Card>
   );
@@ -45,6 +49,11 @@ const styles = {
     alignItems: "center",
     marginLeft: 10,
     marginRight: 10
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
   }
 };
 
