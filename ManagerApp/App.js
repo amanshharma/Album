e1b225557f3a5e2984b09772043f5a5c501b7d1d
Aman Sharma <amandeep.xml@gmail.com>
2019-07-03 00:59:16 -0400

    Added a login form to manager app

--- ManagerApp/App.js
+++ ManagerApp/App.js
@@ -3,13 +3,29 @@ import { createStore } from "redux";
 import { Provider } from "react-redux";
 import { View, Text } from "react-native";
 import reducers from "./src/reducers";
+import firebase from "firebase";
+import LoginForm from "./src/components/LoginForm";
 
 class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: "AIzaSyC_EyC0IByAv1n2JomWB-tS1FYi6rG52nI",
+      authDomain: "manager-15232.firebaseapp.com",
+      databaseURL: "https://manager-15232.firebaseio.com",
+      projectId: "manager-15232",
+      storageBucket: "manager-15232.appspot.com",
+      messagingSenderId: "640057662313",
+      appId: "1:640057662313:web:98b4d6e8e97d3288"
+    };
+
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
       <Provider store={createStore(reducers)}>
         <View>
-          <Text>Heyy</Text>
+          <LoginForm />
         </View>
       </Provider>
     );
---
+++ ManagerApp/src/components/LoginForm.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import { Text } from "react-native";
+import { connect } from "react-redux";
+//import { emailChanged, passwordChanged, loginUser } from '../actions';
+import { Card, CardSection, Input, Button, Spinner } from "../common";
+
+class LoginForm extends Component {
+  render() {
+    return (
+      <Card>
+        <CardSection>
+          <Input label="Email" placeholder="Email id" />
+        </CardSection>
+        <CardSection>
+          <Input secureTextEntry label="Password" placeholder="Password" />
+        </CardSection>
+        <CardSection>
+          <Button>Login</Button>
+        </CardSection>
+      </Card>
+    );
+  }
+}
+
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: "center",
+    color: "red"
+  }
+};
+
+export default LoginForm;
