9ec4f57b1edb48f3f856f3005b0acae663ff2fce
Aman Sharma <amandeep.xml@gmail.com>
2019-07-03 21:01:10 -0400

    Added reducers and actions

--- ManagerApp/src/components/LoginForm.js
+++ ManagerApp/src/components/LoginForm.js
@@ -1,15 +1,21 @@
 import React, { Component } from "react";
 import { Text } from "react-native";
 import { connect } from "react-redux";
-//import { emailChanged, passwordChanged, loginUser } from '../actions';
+import { emailChanged, passwordChanged, loginUser } from "../actions";
 import { Card, CardSection, Input, Button, Spinner } from "../common";
 
 class LoginForm extends Component {
+  onEmailChange = text => {};
+
   render() {
     return (
       <Card>
         <CardSection>
-          <Input label="Email" placeholder="Email id" />
+          <Input
+            label="Email"
+            placeholder="Email id"
+            onChangeText={this.onEmailChange}
+          />
         </CardSection>
         <CardSection>
           <Input secureTextEntry label="Password" placeholder="Password" />
---
+++ ManagerApp/src/reducers/AuthReducer.js
@@ -0,0 +1,10 @@
+import { EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER } from "../actions/types";
+const INITIAL_STATE = { email: "" };
+
+export default (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case EMAIL_CHANGED:
+
+    default:
+  }
+};
